b9b12cb5c3e1c56830081603709bac1903667c45
Angel Sullon <asullom@gmail.com>
2018-04-19 16:02:00 -0500

    ejercicio2 con Double

--- Ejer02.java
+++ Ejer02.java
@@ -11,9 +11,9 @@ public class Ejer02{
 			precioPagar= precioSinOfe - precioSinOfe*0.22
 		*/
 		Scanner reader = new Scanner(System.in);
-		int precioSinOfe = 0;
+		double precioSinOfe = 0;
 		System.out.print("precioSinOfe:");
-		precioSinOfe = reader.nextInt();
+		precioSinOfe = Double.parseDouble( reader.nextLine() );
 		double precioPagar= precioSinOfe - precioSinOfe*0.22;
 		System.out.print("precioPagar:"+precioPagar);
 	}
